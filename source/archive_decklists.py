3a3119a0dba05c5767b9dc99f3dc6d6091e546da
Simon <simonmurphy@bluebridgetech.com>
2019-01-03 22:12:07 +0000

    Updated to work for multiple users.

--- source/archive_decklists.py
+++ source/archive_decklists.py
@@ -17,29 +17,33 @@ page = pages.Home(browser)
 page.navigate_to_home()
 page.accept_cookies()
 
-username = "ReggTheSecond"
+usernames = [
+    "ReggTheSecond",
+    "Karab"
+]
 
-page = pages.UserDecklists(browser)
-decknames_and_deck_urls = page.get_users_decklists_names_for_user(username)
-page = pages.DeckList(browser)
+for username in usernames:
+    page = pages.UserDecklists(browser)
+    decknames_and_deck_urls = page.get_users_decklists_names_for_user(username)
+    page = pages.DeckList(browser)
 
-try:
-    os.mkdir(get_cwp() + "/data/")
-except Exception:
-    pass
+    try:
+        os.mkdir(get_cwp() + "/data/")
+    except Exception:
+        pass
 
 
-for key in decknames_and_deck_urls:
-    print key
-    file = open(get_cwp() + "/data/" + key + ".txt", 'w')
-    date = datetime.now()
-    deck_info = "Deck Name: " + key +\
-        "\nUsername: " + username +\
-        "\nDate: " + date.strftime("%d/%b/%Y") +\
-        "\n"
-    browser.get(decknames_and_deck_urls[key])
-    decklist = page.get_list_of_cards_in_deck()
-    file.write(deck_info + decklist)
-    file.close()
+    for key in decknames_and_deck_urls:
+        print key
+        file = open(get_cwp() + "/data/" + key + ".txt", 'w')
+        date = datetime.now()
+        deck_info = "Deck Name: " + key +\
+            "\nUsername: " + username +\
+            "\nDate: " + date.strftime("%d/%b/%Y") +\
+            "\n"
+        browser.get(decknames_and_deck_urls[key])
+        decklist = page.get_list_of_cards_in_deck()
+        file.write(deck_info + decklist)
+        file.close()
 
 browser.quit()
